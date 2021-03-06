07f2f37bf2f09e4ce2cbef1c808a9a8144c1a070
Anthony Fisher <apf312@nyu.edu>
2021-03-06 13:57:47 -0800

    Merged my work into the main chatbot.

--- chatbot_proj/actions/actions.py
+++ chatbot_proj/actions/actions.py
@@ -25,9 +25,16 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
-from rasa_sdk import Action
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 from rasa_core.events import AllSlotsReset
 from rasa_core.events import Restarted
+from datetime import datetime
+import random
 
 class ActionRestarted(Action): 	
     def name(self): 		
@@ -39,3 +46,64 @@ class ActionSlotReset(Action):
         return 'action_slot_reset' 	
     def run(self, dispatcher, tracker, domain): 		
         return[AllSlotsReset()]
+        
+class ActionReportCurrentTime(Action):
+    
+    def name(self) -> Text:
+        return "action_report_current_time"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M")
+        # print("Current Time =", current_time)
+        output = "It's " + current_time
+        
+        dispatcher.utter_message(text=output)
+        
+        return []
+        
+class ActionCheckMail(Action):
+    
+    def name(self) -> Text:
+        return "action_check_mail"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        hasMail = bool(random.getrandbits(1))
+        # hasMail = False
+        print("Checked mail: ", hasMail)
+        
+        
+        return [SlotSet("has_mail", hasMail)]
+        
+class ActionResetAirport(Action):
+
+    def name(self) -> Text:
+        return "action_reset_airport"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        return [SlotSet("airport", None)]
+        
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            
+        output = "Hello World!"
+
+        dispatcher.utter_message(text=output)
+
+        return []
